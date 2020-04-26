1daec38001c3edfaf24fd9f03b452811005ebd60
Sim410on <02fahsim@rgtfo-me.it>
2020-04-26 18:08:52 +0200

    Binear zu Dezimal

--- ZahlensystemATeam/ZahlensystemATeam/Program.cs
+++ ZahlensystemATeam/ZahlensystemATeam/Program.cs
@@ -7,10 +7,29 @@ namespace ZahlensystemATeam
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
             // des sich a kommentar
+            string Binärzahl = "10010011";
 
+
+            int ErgebnisDEZ = DezimalUmrechnen(Binärzahl);
+
+            Console.WriteLine(ErgebnisDEZ);
+            Console.ReadKey();
         }
+
+        static int DezimalUmrechnen(string Bin)
+        {
+
+            string Dezimal = Convert.ToInt32(Bin, 2).ToString();
+
+            int Dezimalzahl = Convert.ToInt32(Dezimal);
+
+            return Dezimalzahl;
+        }
+
     }
 }
